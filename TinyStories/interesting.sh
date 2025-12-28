f33ba89579b0a74427614118314eb03454d10189
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2025-12-28 22:24:03 +0100

    Avoid crash on memref.global without dense initializer

--- TinyStories/interesting.sh
+++ TinyStories/interesting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -u
 
-/home/roland/circt-nix/result/bin/circt-opt -verify-each -flatten-memref $1
+/home/roland/circt-nix/result/bin/circt-opt -verify-each -mlir-disable-threading -flatten-memref $1
 rc=$?
 
 # interesting = failure/crash
